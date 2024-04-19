00d9e8d5d6987ebaa41403aaf7f0a333c4421fb6
bogdanoprea1 <bogdanopreaprim@gmail.com>
2024-04-19 12:44:28 +0300

    Update Vite configuration for correct base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/gim-react/',
   plugins: [react()],
   build: {
     outDir: 'build',  // Change this line if it's set to 'dist'
